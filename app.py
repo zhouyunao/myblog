ad94c342c10ad57c493093befe80c9a0cd55a6a6
ingo <yunaozhou@gmail.com>
2019-02-03 23:19:40 +0900

    make it able to write new post in markdown.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_simplemde import SimpleMDE
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/ingo/Documents/myblog/blog.db'
+# app.config['SIMPLEMDE_JS_IIFE'] = True
+# app.config['SIMPLEMDE_USE_CDN'] = True
+SimpleMDE(app)
+db = SQLAlchemy(app)
 
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 @app.route('/')
 def index():
@@ -10,6 +24,10 @@ def index():
 @app.route('/post')
 def post():
     return render_template('post.html')
+
+@app.route('/new')
+def new_post():
+    return render_template('new.html')
 
 @app.route('/contact')
 def contact():
